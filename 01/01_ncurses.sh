a0846bf6af129da85497498ab9656f69a7c62779
th1nhhdk <th1nhhdk@tutanota.com>
2022-10-06 04:01:00 +0000

    Added ncurses

---
+++ 01/01_ncurses.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -ex
+PKGNAME=ncurses
+PKGVER=6.3
+ARCHIVEDIR=$PKGNAME-$PKGVER
+ARCHIVENAME=$ARCHIVEDIR.tar.gz
+
+pushd $LFS/sources
+  tar -xf $ARCHIVENAME
+  pushd $ARCHIVEDIR
+    sed -i s/mawk// configure
+    mkdir build
+    pushd build
+      ../configure
+      make -C include
+      make -C progs tic
+    popd
+    ./configure --prefix=/usr                \
+	        --host=$LFS_TGT              \
+		--build=$(./config.guess)    \
+		--mandir=/usr/share/man      \
+		--with-manpage-format=normal \
+		--with-shared                \
+		--without-normal             \
+		--with-cxx-shared            \
+		--without-debug              \
+		--without-ada                \
+		--disable-stripping          \
+		--enable-widec
+    make
+    make DESTDIR=$LFS TIC_PATH=$(pwd)/build/progs/tic install
+    echo "INPUT(-lncursesw)" > $LFS/usr/lib/libncurses.so
+  popd
+  rm -rf $ARCHIVEDIR
+popd
