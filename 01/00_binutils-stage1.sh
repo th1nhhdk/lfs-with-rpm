74a888d22aa1e19608da316e6053e02eb8b72b16
th1nhhdk <th1nhhdk@tutanota.com>
2022-10-06 03:01:18 +0000

    Updated script

---
+++ 01/00_binutils-stage1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -ex
+PKGNAME=binutils
+PKGVER=2.39
+ARCHIVEDIR=$PKGNAME-$PKGVER
+ARCHIVENAME=$ARCHIVEDIR.tar.xz
+
+pushd $LFS/sources
+  tar -xf $ARCHIVENAME
+  pushd $ARCHIVEDIR
+    mkdir build
+    cd build
+    ../configure --prefix=$LFS/tools \
+		 --with-sysroot=$LFS \
+		 --target=$LFS_TGT   \
+		 --disable-nls       \
+		 --enable-gprofng=no \
+		 --disable-werror
+    make
+    make install
+  popd
+  rm -rf $ARCHIVEDIR
+popd
